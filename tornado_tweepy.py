a71a8fec106acfc986753a1aae82bd2e6cb73fae
Avtar Khalsa <akhalsa43@gmail.com>
2015-08-18 08:22:59 -0500

    try running a single socket

--- tornado_tweepy.py
+++ tornado_tweepy.py
@@ -58,10 +58,11 @@ class StdOutListener(tweepy.StreamListener):
         
 class HandleListener(tweepy.StreamListener):
         def __init__(self):
-                self.setupSocket()
+                thread.start_new_thread( self.setupSocket, ( ) )
 
                 
         def setupSocket(self, ):
+                print "starting to set up socket listen on new thread"
                 auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UNrVbqEEctj4AiJYFR6J1hDQTXEQK')
                 auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
                 stream = tweepy.Stream(auth, self)
