54d08b2972bee18ba6681347b5f6b0bf1d1b57e2
Avtar Khalsa <akhalsa43@gmail.com>
2015-09-19 13:54:53 -0500

    use oath

--- test_bot.py
+++ test_bot.py
@@ -2,7 +2,14 @@
 
 import tweepy
 
+key = "48jw2ouXpj6AjwLb6BraCZ9cV"
+secret = "vtRB7iNur213gIW5qol6A79vZFUAuhDcL34SmnYFuBRkATweco"
 
-auth = tweepy.BasicAuthHandler("FiltraSports", "filtra123")
+access_token = "75fs4ijhWnt2Ss9AkJKsZLQUSd0lRWyfDsVGYKgxc"
+token_secret = "P6KajgWHX0eNkEYnI4bGevrroWW9uHuzohl5Gl5uLv6UT"
+
+
+auth = tweepy.OAuthHandler(key, secret)
+auth.set_access_token(access_token, token_secret)
 api = tweepy.API(auth)
-api.update_status('hello from tweepy!')
+api.update_status('hello from tweepy library!')
