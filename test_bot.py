0c4dd95c997b9fc87c91deb56239877467ce4867
Avtar Khalsa <akhalsa43@gmail.com>
2015-09-19 13:56:36 -0500

    fix creds

--- test_bot.py
+++ test_bot.py
@@ -5,7 +5,7 @@ import tweepy
 key = "48jw2ouXpj6AjwLb6BraCZ9cV"
 secret = "vtRB7iNur213gIW5qol6A79vZFUAuhDcL34SmnYFuBRkATweco"
 
-access_token = "75fs4ijhWnt2Ss9AkJKsZLQUSd0lRWyfDsVGYKgxc"
+access_token = "24662514-75fs4ijhWnt2Ss9AkJKsZLQUSd0lRWyfDsVGYKgxc"
 token_secret = "P6KajgWHX0eNkEYnI4bGevrroWW9uHuzohl5Gl5uLv6UT"
 
 
