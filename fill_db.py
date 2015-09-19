cfe7812bf8f67590f30de5354ad30f21701315ea
Avtar Khalsa <akhalsa43@gmail.com>
2015-09-19 09:08:28 -0500

    get ready to populate DB w profiles

---
+++ fill_db.py
@@ -0,0 +1,33 @@
+#populate profile picture url
+
+import MySQLdb
+
+
+auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UNrVbqEEctj4AiJYFR6J1hDQTXEQK')
+auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
+api = tweepy.API(auth)
+
+db = MySQLdb.connect(
+        host="avtar-news-db-2.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com",
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
+        
+
+def getAllTwitterIds():
+        print "attempting to acquire lock at getAll187"
+        lock.acquire()
+        print "successfully acquired lock at 187"
+        cursor = db.cursor()
+        sql = "SELECT twitter_id FROM TwitterSource;"
+        cursor.execute(sql)
+        return_list = []
+        for row in cursor.fetchall():
+                return_list.append(row[0])
+        cursor.close()
+        lock.release()
+        return return_list
+
+print getAllTwitterIds()
