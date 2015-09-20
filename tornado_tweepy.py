1fe7385012205bf45913ce737a161e5b2b5da22d
Avtar Khalsa <akhalsa43@gmail.com>
2015-09-20 18:35:21 -0500

    only check the most recent 30 seconds worth of occurrences

--- tornado_tweepy.py
+++ tornado_tweepy.py
@@ -198,29 +198,30 @@ def addOccurance(tweet_id):
         cursor.close()
         
         
-        # cursor = db.cursor()
-        # sql = "SELECT * from TweetOccurrence WHERE twitter_id LIKE '"+str(tweet_id)+"';"
-        # cursor.execute(sql)
-        # occurrence_count = cursor.rowcount
-        # cursor.close()
-        # 
-        # if(occurrence_count == 150):
-        #         cursor = db.cursor()
-        #         sql = "SELECT * From Tweet where twitter_id like '"+str(tweet_id)+"';"
-        #         cursor.execute(sql)
-        #         for row in cursor.fetchall():
-        #                 #return_id = row[0]
-        #                 print "************** RETWEET************"
-        #                 print row
-        #                 print "****************END RETWEET ******"
-        #                 print "time since start"
-        #                 delta_time = datetime.datetime.now() - row[4]
-        #                 #print "delta_time seconds: "+ str(delta_time.total_seconds())
-        #                 if(delta_time.total_seconds() < 30):
-        #                         api_bot.retweet(tweet_id)
-        #                 
-        #         #api.update_status(status = 'hello from tweepy library!')
-        #         cursor.close()
+        cursor = db.cursor()
+        sql = "SELECT * from TweetOccurrence WHERE twitter_id LIKE '"+str(tweet_id)+"' AND timestamp > (NOW() - INTERVAL 30 SECOND);"
+
+        cursor.execute(sql)
+        occurrence_count = cursor.rowcount
+        cursor.close()
+         
+        if(occurrence_count == 150):
+                cursor = db.cursor()
+                sql = "SELECT * From Tweet where twitter_id like '"+str(tweet_id)+"';"
+                cursor.execute(sql)
+                for row in cursor.fetchall():
+                        #return_id = row[0]
+                        print "************** RETWEET************"
+                        print row
+                        print "****************END RETWEET ******"
+                        print "time since start"
+                        delta_time = datetime.datetime.now() - row[4]
+                        #print "delta_time seconds: "+ str(delta_time.total_seconds())
+                        if(delta_time.total_seconds() < 30):
+                                api_bot.retweet(tweet_id)
+                        
+                #api.update_status(status = 'hello from tweepy library!')
+                cursor.close()
         print "addOccurrance took: "+str((datetime.datetime.now() - addOccurance_start).total_seconds())+" seconds" 
         lock.release()
 
