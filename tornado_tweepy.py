34fdc52a37d3681adf4a0bda3cb7b9c547ef8864
Avtar Khalsa <akhalsa43@gmail.com>
2015-09-20 12:12:27 -0500

    casts

--- tornado_tweepy.py
+++ tornado_tweepy.py
@@ -182,7 +182,7 @@ def addOccurance(tweet_id):
                         print "****************END RETWEET ******"
                         print "time since start"
                         delta_time = datetime.datetime.now() - row[4]
-                        print "delta_time seconds: "+ delta_time.total_seconds()
+                        print "delta_time seconds: "+ str(delta_time.total_seconds())
                         api_bot.retweet(tweet_id)
                         
                 #api.update_status(status = 'hello from tweepy library!')
