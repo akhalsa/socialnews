a3eaa2e3b9a7efdeafa0c8afd23eb0660b1c7f9c
Avtar Khalsa <akhalsa43@gmail.com>
2015-09-20 13:43:02 -0500

    wrap in try-except

--- tornado_tweepy.py
+++ tornado_tweepy.py
@@ -50,7 +50,11 @@ class HandleListener(tweepy.StreamListener):
                 auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UNrVbqEEctj4AiJYFR6J1hDQTXEQK')
                 auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
                 stream = tweepy.Stream(auth, self)
-                stream.filter(follow=getAllTwitterIds())
+                try:
+                        stream.filter(follow=getAllTwitterIds())
+                except ProtocolError, e:
+                        print "Handling protocol exception"
+                        self.setupSocket()
                 
         def on_data(self, data):
                 decoded = json.loads(data)
