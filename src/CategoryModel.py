48a5e62f24b71102fe8cb5618705071533d94983
Avtar Khalsa <akhalsa43@gmail.com>
2015-10-26 00:34:41 -0500

    maybe array?

--- src/CategoryModel.py
+++ src/CategoryModel.py
@@ -5,5 +5,7 @@ class CategoryModel:
     def __init__(self):
         print os.getcwd()
         obj = untangle.parse('handles.xml')
-        print obj.root.category['name']
+        print obj.root.category
+        for cat in obj.root.category.category:
+            print cat['name']
         print "initializing a catgory model"
