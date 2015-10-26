e4265f48c8a8869efa5c13067a587a563e5ff951
Avtar Khalsa <akhalsa43@gmail.com>
2015-10-25 23:42:38 -0500

    shift to categorymodule

--- tornado_tweepy.py
+++ tornado_tweepy.py
@@ -612,7 +612,7 @@ app = tornado.web.Application([
 ])
 
 if __name__ == '__main__':
-    src.test_method("Test Of Category Model")
+    src.CategoryModule.test_method("Test Of Category Model")
     parse_command_line()
     #db.query('SET GLOBAL wait_timeout=28800')
     handle = HandleListener()
