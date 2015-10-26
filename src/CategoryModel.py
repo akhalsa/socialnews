85fbbc68e22234dba94a1a8967be7ed2954b2143
Avtar Khalsa <akhalsa43@gmail.com>
2015-10-26 00:36:41 -0500

    is this printing cats?

--- src/CategoryModel.py
+++ src/CategoryModel.py
@@ -5,7 +5,7 @@ class CategoryModel:
     def __init__(self):
         print os.getcwd()
         obj = untangle.parse('handles.xml')
-        print obj.root.category
+        
         for cat in obj.root.category.category.category:
             print cat['name']
         print "initializing a catgory model"
