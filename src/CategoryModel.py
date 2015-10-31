8068e77b71df5a0b3e1b56c25405527ae637fdb4
Avtar Khalsa <akhalsa43@gmail.com>
2015-10-30 20:44:19 -0500

    connect to db

--- src/CategoryModel.py
+++ src/CategoryModel.py
@@ -2,10 +2,25 @@ import untangle
 import MySQLdb
 import os
 
+
+define("port", default=8888, help="run on the given port", type=int)
+
+db = MySQLdb.connect(
+        host="avtar-news-db-2.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com",
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
+
+
+
 class CategoryModel:
-    def __init__(self):
+    def __init__(self, db):
         print os.getcwd()
         obj = untangle.parse('handles.xml')
+        #table naming scheme
+        #GenCat0
         
         for cat in obj.root.category.category.category:
             print cat['name']
