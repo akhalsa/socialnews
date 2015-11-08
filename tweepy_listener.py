2ad0afc651be46938ad771a54a631520aa657e62
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-07 18:53:02 -0600

    good imports bad second auth

--- tweepy_listener.py
+++ tweepy_listener.py
@@ -3,6 +3,7 @@ import thread
 import src.CategoryModel
 import datetime
 import MySQLdb
+import json
 
 from threading import Thread
 from Queue import Queue
@@ -38,8 +39,6 @@ class HandleListener(tweepy.StreamListener):
                 
         def setupSocket(self, ):
                 print "starting to set up socket listen on new thread"
-                auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UNrVbqEEctj4AiJYFR6J1hDQTXEQK')
-                auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
                 stream = tweepy.Stream(auth, self)
                 stream.filter(follow=getAllTwitterIds())
 
