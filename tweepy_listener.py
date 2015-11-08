27b89ec1ea32f0d45a74e91d25b0834aa889117c
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-07 18:18:33 -0600

    include connection

--- tweepy_listener.py
+++ tweepy_listener.py
@@ -7,7 +7,13 @@ from threading import Thread
 from Queue import Queue
 
 
-
+db = MySQLdb.connect(
+        host="avtar-news-db-2.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com",
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
 
 auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UNrVbqEEctj4AiJYFR6J1hDQTXEQK')
 auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
