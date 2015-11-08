314ad60e31d368f61710d83c24cb7d45679e62bb
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-07 18:39:23 -0600

    don't actually need these

--- tornado_tweepy.py
+++ tornado_tweepy.py
@@ -22,13 +22,7 @@ import src.CategoryModel
 
 define("port", default=8888, help="run on the given port", type=int)
 
-db = MySQLdb.connect(
-        host="avtar-news-db-2.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com",
-        user="akhalsa",
-        passwd="sophiesChoice1",
-        db="newsdb",
-        charset='utf8',
-        port=3306)
+
 
 lock = threading.Lock()
 
