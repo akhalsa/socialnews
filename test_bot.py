cbcd813fc02af87418e6a507d0dc88fec0fe72a0
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-19 22:41:26 -0600

    hooray we can listen

--- test_bot.py
+++ test_bot.py
@@ -5,8 +5,23 @@ auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UN
 auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
 api = tweepy.API(auth)
 
+class HandleListener(tweepy.StreamListener):
+    def on_data(self, data):
+        decoded = json.loads(data)
+        print "got tweet"
+        print decoded
+        return True
 
-stream = tweepy.Stream(auth, self)
+    def on_error(self, status):
+        print status
+listen = HandleListener()
+
+stream = tweepy.Stream(auth, listen)
 stream.filter(track=['baseball'])
+
+while True:
+    pass
+
+
 
 
