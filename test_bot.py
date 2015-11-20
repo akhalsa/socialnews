2c290b77413475998523d67b0acdabbef10fe7f4
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-19 23:07:40 -0600

    this should terminate the connection

--- test_bot.py
+++ test_bot.py
@@ -8,17 +8,24 @@ auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6z
 api = tweepy.API(auth)
 
 class HandleListener(tweepy.StreamListener):
+    
+    def __init__(self, timeout):
+        self.timeout = timeout
+        self.start_time = datetime.datetime.now()
     def on_data(self, data):
         decoded = json.loads(data)
         print "got tweet"
         print decoded
-        return True
+        if((datetime.datetime.now() - self.start_time).seconds > self.timeout):
+            return False
+        else:
+            return True
 
     def on_error(self, status):
         print status
 listen = HandleListener()
 
-stream = tweepy.Stream(auth, listen, timeout=20)
+stream = tweepy.Stream(auth, listen)
 stream.filter(track=['baseball'], async=True)
 
 
