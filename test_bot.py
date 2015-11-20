93b5af062c6e6d24da5a3377c00b3ee02c4bc591
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-19 23:00:04 -0600

    don't forget time

--- test_bot.py
+++ test_bot.py
@@ -1,6 +1,7 @@
 import tweepy
 from tweepy import Stream
 import json
+import time
 
 auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UNrVbqEEctj4AiJYFR6J1hDQTXEQK')
 auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
