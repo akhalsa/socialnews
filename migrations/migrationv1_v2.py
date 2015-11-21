7c0b5c9d8acc4eee5cd5d0459ca2f481d75d064c
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-21 17:28:19 -0600

    does this update the db?

--- migrations/migrationv1_v2.py
+++ migrations/migrationv1_v2.py
@@ -9,18 +9,27 @@ host_live = "avtar-news-db-2.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
 host_dev = "avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
 host_target = host_live
 
-
+def executeSql(db, sql):
+        cursor = db.cursor()
+        try:
+                # Execute the SQL command
+                cursor.execute(sql)
+                # Commit your changes in the database
+                db.commit()
+        except Exception,e:
+                # Rollback in case there is any error
+                print "error on insertion of occurrence"
+                print str(e)
+                db.rollback()
+        lastRow = cursor.lastrowid
+        cursor.close()
+        
+        
 def forward():
-    print "got sys arg: "+sys.argv[0]+" and second sys arg: "+sys.argv[1]
-    
     if(sys.argv[1] == "0"):
         host_target = host_live
     elif(sys.argv[1] == "1"):
-        print "got to host_target assignment"
         host_target = host_dev
-    
-    print "found host target"+host_target   
-    global db
     
     db = MySQLdb.connect(
         host=host_target,
@@ -29,3 +38,7 @@ def forward():
         db="newsdb",
         charset='utf8',
         port=3306)
+    
+    sql = "ALTER TABLE TwitterSource ADD `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP;";
+    executeSql(db, sql)
+    
