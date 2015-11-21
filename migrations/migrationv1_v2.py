444e51004d0955ec18af428909f3c6ce575ad0fc
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-21 17:11:18 -0600

    better args maybe?

--- migrations/migrationv1_v2.py
+++ migrations/migrationv1_v2.py
@@ -1,5 +1,5 @@
 #This is a migration from 7a9e15e830e7b82856 to next commit
-#import MySQLdb
+import MySQLdb
 
 import sys
 
@@ -11,19 +11,19 @@ host_target = host_live
 
 
 def forward():
-    print "got sys arg: "+sys.argv[1]
+    print "got sys arg: "+sys.argv[0]
     
-    # if(options.mysql_host == 0):
-    #     host_target = host_live
-    # elif(options.mysql_host == 1):
-    #     host_target = host_dev
-    #     
-    # global db
-    # 
-    # db = MySQLdb.connect(
-    #     host=host_target,
-    #     user="akhalsa",
-    #     passwd="sophiesChoice1",
-    #     db="newsdb",
-    #     charset='utf8',
-    #     port=3306)
+    if(options.mysql_host == 0):
+        host_target = host_live
+    elif(options.mysql_host == 1):
+        host_target = host_dev
+        
+    global db
+    
+    db = MySQLdb.connect(
+        host=host_target,
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
