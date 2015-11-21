072dfaafe284daa7e273f74fa47498c9fd6ac66d
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-21 17:33:12 -0600

    include drop column

--- migrations/migrationv1_v2.py
+++ migrations/migrationv1_v2.py
@@ -42,3 +42,19 @@ def forward():
     sql = "ALTER TABLE TwitterSource ADD `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP;";
     executeSql(db, sql)
     
+def backward():
+    if(sys.argv[1] == "0"):
+        host_target = host_live
+    elif(sys.argv[1] == "1"):
+        host_target = host_dev
+    
+    db = MySQLdb.connect(
+        host=host_target,
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
+
+    sql = "ALTER TABLE TwitterSource DROP COLUMN `timestamp`;";
+    executeSql(db, sql)
