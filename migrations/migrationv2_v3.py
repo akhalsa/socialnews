38d550c583da65f9ef80a2a6eb914afb4b96f3e6
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-22 00:10:03 -0600

    set up db migration

---
+++ migrations/migrationv2_v3.py
@@ -0,0 +1,82 @@
+#This is a migration from 7a9e15e830e7b82856 to next commit
+import MySQLdb
+
+import sys
+
+
+
+host_live = "avtar-news-db-2.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
+host_dev = "avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
+host_target = host_live
+
+def executeSql(db, sql):
+        cursor = db.cursor()
+        try:
+                # Execute the SQL command
+                cursor.execute(sql)
+                # Commit your changes in the database
+                db.commit()
+        except Exception,e:
+                # Rollback in case there is any error
+                print "error on insertion of occurrence"
+                print str(e)
+                db.rollback()
+        lastRow = cursor.lastrowid
+        cursor.close()
+        
+        
+def forward():
+    if(sys.argv[1] == "0"):
+        host_target = host_live
+    elif(sys.argv[1] == "1"):
+        host_target = host_dev
+    
+    db = MySQLdb.connect(
+        host=host_target,
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
+    
+    sql = "CREATE TABLE `VoteHistory` (";
+    sql += "`ID` int(11) NOT NULL AUTO_INCREMENT,"
+    sql += "`ip_address` varchar(255) DEFAULT NULL,"
+    sql += "`category_id` INT DEFAULT NULL,"
+    sql += "`source_id` INT DEFAULT NULL,"
+    sql += "`timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,"
+    sql += "'value' INT DEFAULT NULL"
+    sql += "PRIMARY KEY (`ID`));"
+    executeSql(db, sql)
+    
+    sql += "DROP TABLE SourceCategoryRelationship;"
+    executeSql(db, sql)
+    
+    
+    
+    
+def backward():
+    if(sys.argv[1] == "0"):
+        host_target = host_live
+    elif(sys.argv[1] == "1"):
+        host_target = host_dev
+    
+    db = MySQLdb.connect(
+        host=host_target,
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
+
+    sql = "DROP TABLE VoteHistory;";
+    executeSql(db, sql)
+    
+    sql = "CREATE TABLE `SourceCategoryRelationship` ("
+    sql += "`ID` int(11) NOT NULL AUTO_INCREMENT,"
+    sql += "`source_id` int(11) DEFAULT NULL,"
+    sql += "`category_id` int(11) DEFAULT NULL,"
+    sql += "PRIMARY KEY (`ID`))"
+    
+    executeSql(db, sql)
+    
