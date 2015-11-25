07e0af8488063c2f2aa9769f0033f5ca600240b8
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-25 17:35:32 -0600

    this is probably the right way to do imports

--- src/CategoryModel.py
+++ src/CategoryModel.py
@@ -2,8 +2,7 @@ import untangle
 import MySQLdb
 import os
 import re
-import tweepy
-from DBWrapper import *
+from tornado.options import define, options, parse_command_line
 
 auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UNrVbqEEctj4AiJYFR6J1hDQTXEQK')
 auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
