34b26c1c3c26406cfdd57b37f577bf9f4b698ddb
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-24 22:17:33 -0600

    properly value score

--- src/DBWrapper.py
+++ src/DBWrapper.py
@@ -492,7 +492,7 @@ def reloadSourceCategoryRelationship(local_db):
         cursor = local_db.cursor()
         cat_id = row[0]
         #simple algorithm... lets just start with the top 30 voted handles ... this may get more complex later
-        sql = "SELECT twitter_id, COUNT(twitter_id) as vote_count FROM VoteHistory WHERE category_id like "+str(cat_id)+" GROUP BY twitter_id ORDER BY vote_count DESC;"
+        sql = "SELECT twitter_id, SUM(value) as vote_count FROM VoteHistory WHERE category_id like "+str(cat_id)+" GROUP BY twitter_id ORDER BY vote_count DESC;"
         
         cursor.execute(sql)
         total_nominated_handles = cursor.rowcount
