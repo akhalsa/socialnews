6fb6c6c6f85fad8ffc28dd268e6aeca5c0362d97
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-25 17:32:33 -0600

    first pass at creating fixture data

--- src/CategoryModel.py
+++ src/CategoryModel.py
@@ -2,19 +2,19 @@ import untangle
 import MySQLdb
 import os
 import re
+import tweepy
 from src.DBWrapper import *
 
 auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UNrVbqEEctj4AiJYFR6J1hDQTXEQK')
 auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
 api = tweepy.API(auth)
 
-db = MySQLdb.connect(
-        host=host_target,
-        user="akhalsa",
-        passwd="sophiesChoice1",
-        db="newsdb",
-        charset='utf8',
-        port=3306)
+
+
+host_live = "avtar-news-db-2.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
+host_dev = "avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
+host_target = host_live
+
 
 class CategoryModel:
     
@@ -177,6 +177,20 @@ class CategoryModel:
             
             
 if __name__ == '__main__':
+    parse_command_line()
+    if(options.mysql_host == 0):
+        host_target = host_live
+    elif(options.mysql_host == 1):
+        host_target = host_dev
+        
+    db = MySQLdb.connect(
+        host=host_target,
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
+    
     mdl = CategoryModel(db, api)
         
     
