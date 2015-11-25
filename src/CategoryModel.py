1ceb186e534544e48bdf72510a6dee177ede1337
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-25 17:34:11 -0600

    remove src. so we can import directly?

--- src/CategoryModel.py
+++ src/CategoryModel.py
@@ -3,7 +3,7 @@ import MySQLdb
 import os
 import re
 import tweepy
-from src.DBWrapper import *
+from DBWrapper import *
 
 auth = tweepy.OAuthHandler('pxtsR83wwf0xhKrLbitfIoo5l', 'Z12x1Y7KPRgb1YEWr7nF2UNrVbqEEctj4AiJYFR6J1hDQTXEQK')
 auth.set_access_token('24662514-MCXJydvx0Mn5GWfW7RqQmXXsu35m8rNmzxKfHYJcM', 'f6zSrTomKIIr2c5zwcbkpbJYSpAZ2gi40yp57DEd86enN')
