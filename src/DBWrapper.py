cbd120e6361455c2ab5a11134ef259e98aade28c
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-27 14:38:50 -0600

    does this do everything in unicode?

--- src/DBWrapper.py
+++ src/DBWrapper.py
@@ -104,7 +104,7 @@ def getTweetOccurances(seconds, cat_id, local_db):
                         
                         img_url = img_url.encode('utf-8')
                         url = url.encode('utf-8')
-                        title = unicode(re.escape(soup.title.string), 'utf-8')
+                        title = re.escape(soup.title.string).encode('utf-8')
                         blurb_text = ""
                         
                         for potential_desc in desc:
@@ -130,7 +130,7 @@ def getTweetOccurances(seconds, cat_id, local_db):
                         else:
                             print "title isnt unicode"
                             
-                        sql = "UPDATE Tweet SET blurb=\""+blurb_text+"\", link_url=\""+url+"\", link_text=\""+title+"\", "
+                        sql = u"UPDATE Tweet SET blurb=\""+blurb_text+u"\", link_url=\""+url+u"\", link_text=\""+title+"\", "
                         
                         second_sql = "img_url=\""+img_url+"\", checked=1 WHERE twitter_id like '"+tweet_dict["id"]+"';"
                         
