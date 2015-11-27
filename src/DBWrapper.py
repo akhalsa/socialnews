7736aa5ef06c30f9d9d8bb1543f6ac350eb704d3
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-27 13:54:20 -0600

    is it the other line?

--- src/DBWrapper.py
+++ src/DBWrapper.py
@@ -114,8 +114,9 @@ def getTweetOccurances(seconds, cat_id, local_db):
                         
                         print img_url
                         print "img url: "+img_url
-                        print "tweet_dict id"+tweet_dict["id"]
+                        print "tweet_dict id: "+tweet_dict["id"]
                         sql = "UPDATE Tweet SET blurb=\""+re.escape(blurb_text)+"\", link_url=\""+url+"\", link_text=\""+re.escape(title)
+                        print "got update text: "+sql
                         sql += "\", img_url=\""+img_url+"\", checked=1 WHERE twitter_id like '"+tweet_dict["id"]+"';"
                         insertion_cursor = local_db.cursor()
                         try:
