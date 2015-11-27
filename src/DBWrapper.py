7466c5c27f7a83611c41554792991a5f90d7f2e8
Avtar Khalsa <akhalsa43@gmail.com>
2015-11-27 15:36:17 -0600

    should use default parser this way

--- src/DBWrapper.py
+++ src/DBWrapper.py
@@ -63,7 +63,7 @@ def getTweetOccurances(seconds, cat_id, local_db):
                     if(len(urls) > 0):
                         url = urls[0]
                         page_content = urllib2.urlopen(url).read(200000)
-                        soup = BeautifulSoup(page_content, 'lxml')
+                        soup = BeautifulSoup(page_content)
                         body = soup.find('body')
                         desc = soup.findAll(attrs={"name":"description"})
                         
