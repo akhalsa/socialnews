5eb9ef75660ed9e8f84e2fb151b89702e7cb195d
Rachel Senterfeit <senterfeitrd@gmail.com>
2015-12-30 14:59:42 -0500

    first pass at filtra w angular

--- static/js/filtraCtrl.js
+++ static/js/filtraCtrl.js
@@ -1,4 +1,11 @@
 app.controller("myCtrl", function($scope) {
-    $scope.firstName	= "John";
-    $scope.lastName= "Doe";
+    $scope.selected_top_index = 0;
+    $scope.selected_secondary_index = -1;
+    $scope.selected_third_index = -1;
+    $scope.category_structure = [];
+    
+    $http.get("/category")
+    .then(function(response) {
+        $scope.category_structure = response.data;
+    });
 });
