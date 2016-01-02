3390202bc3271daf86815d47a5f411c3c13f9d0f
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-01 22:36:24 -0600

    first breadcrumb pass

--- static/js/filtraCtrl.js
+++ static/js/filtraCtrl.js
@@ -3,6 +3,7 @@ app.controller("filtraCtrl", function($scope, $http) {
     $scope.selected_secondary_index = -1;
     $scope.selected_third_index = -1;
     $scope.category_structure = [];
+    $scope.current_path = [];
     $scope.time_frames = [{seconds:900, text:"Past 15 Minutes"}, {seconds:3600, text:"Past Hour"},
                            {seconds:10800, text:"Past 3 Hours"}, {seconds:43200, text:"Today"}];
     $scope.selected_time = 0;
@@ -14,6 +15,7 @@ app.controller("filtraCtrl", function($scope, $http) {
         for (index = 0; index < response.data.length; ++index) {
             console.log(response.data[index].name);
         }
+        $scope.current_path = [$scope.category_structure[$scope.selected_top_index]];
     });
     
     $scope.selectionChange = function(top, second, third) {
@@ -24,9 +26,18 @@ app.controller("filtraCtrl", function($scope, $http) {
         $scope.selected_secondary_index = second;
         $scope.selected_third_index = third;
         
+        if ($scope.selected_secondary_index == -1) {
+            $scope.current_path = [$scope.category_structure[$scope.selected_top_index]];
+        } else if ($scope.selected_third_index == -1) {
+            $scope.current_path = [$scope.category_structure[$scope.selected_top_index], $scope.category_structure[$scope.selected_secondary_index] ]
+        } else{
+            $scope.current_path = [$scope.category_structure[$scope.selected_top_index], $scope.category_structure[$scope.selected_secondary_index], $scope.category_structure[$scope.selected_third_index] ]
+        }
+        
     }
     
     $scope.timeChange = function(new_time_index){
         $scope.selected_time = new_time_index;
     }
+    
 });
