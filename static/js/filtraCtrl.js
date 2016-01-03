e58f2ab696900134a6a13ab2d8c1dc9a90145e67
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-03 00:14:00 -0600

    if you vote near a tweet well reload the list

--- static/js/filtraCtrl.js
+++ static/js/filtraCtrl.js
@@ -54,6 +54,9 @@ app.controller("filtraCtrl", function($scope, $http) {
         $scope.selected_time = new_time_index;
         loadTweets();
     }
+    $scope.refreshHandles = function(){
+        loadHandles();
+    }
     
     
     $scope.voteForHandle = function(handle_string, value){
