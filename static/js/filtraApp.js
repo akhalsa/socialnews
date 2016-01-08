477b835f9cb4bd61ab973b53304a4af160bdb62e
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-08 09:52:46 -0600

    old filter.js

--- static/js/filtraApp.js
+++ static/js/filtraApp.js
@@ -1,24 +1,23 @@
-(function(){
-    var app = angular.module("filtraApp", ['ui.bootstrap']);
+
+var app = angular.module("filtraApp", ['ui.bootstrap']);
 
 
-    app.filter('unsafeLink', function($sce) {
-        return function(text) {
-            var urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
-            return $sce.trustAsHtml(text.replace(urlRegEx,"<a href='$1' target=\"_blank\" onclick=\"trackOutboundLink('$1');\">$1</a>"));
-        };
-    });
-    
-    
-    app.filter('matchTwitterName', function(){
-        return function(handles, name){
-            var out = [];
-            for(i=0; i<handles.length; i++){
-                if (handles[i].username == name) {
-                    out.push(handles[i]);
-                }
+app.filter('unsafeLink', function($sce) {
+    return function(text) {
+        var urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
+        return $sce.trustAsHtml(text.replace(urlRegEx,"<a href='$1' target=\"_blank\" onclick=\"trackOutboundLink('$1');\">$1</a>"));
+    };
+});
+
+
+app.filter('matchTwitterName', function(){
+    return function(handles, name){
+        var out = [];
+        for(i=0; i<handles.length; i++){
+            if (handles[i].username == name) {
+                out.push(handles[i]);
             }
-            return out;
         }
-    });
-})();
+        return out;
+    }
+});
