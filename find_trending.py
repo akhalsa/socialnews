090ff5c6a903fdb531fb903aa2242792b3bd9979
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-15 22:29:03 -0600

    loop with some additional logic

--- find_trending.py
+++ find_trending.py
@@ -20,59 +20,83 @@ if __name__ == '__main__':
        host_target = host_dev
        
    
+   finished = False
    
-   local_db_cats = MySQLdb.connect(
-                host=host_target,
-                user="akhalsa",
-                passwd="sophiesChoice1",
-                db="newsdb",
-                charset='utf8',
-                port=3306)
-
-   all_ids = getAllCategoryIds(local_db_cats)
-   for cat_id in all_ids:
-      
-      tweets = getTweetOccurances(900, cat_id, local_db_cats)
-      tweet_array = []
-      for tweet in tweets:
-         tweet_array.append(tweet["text"])
+   while(not finished):
+      local_db_cats = MySQLdb.connect(
+                   host=host_target,
+                   user="akhalsa",
+                   passwd="sophiesChoice1",
+                   db="newsdb",
+                   charset='utf8',
+                   port=3306)
+   
+      all_ids = getAllCategoryIds(local_db_cats)
+      for cat_id in all_ids:
          
-         
-      if len(tweet_array) < 10:
-         continue
-      
-      texts = [[word for word in document.lower().split() if len(word) > 4] for document in tweet_array]
-         
-      frequency = defaultdict(int)
-      for text in texts:
-         for token in text:
-            frequency[token] += 1
+         tweets = getTweetOccurances(900, cat_id, local_db_cats)
+         tweet_array = []
+         total_count = 0
+         for tweet in tweets:
+            tweet_array.append(tweet["text"])
+            total_count += tweet["tweet_count"]
             
-      texts = [[token for token in text if frequency[token] > 1] for text in texts]
-      
-      dictionary = corpora.Dictionary(texts)
-
-      corpus = [dictionary.doc2bow(text) for text in texts]
-
-      tfidf = models.TfidfModel(corpus)
-      
-      corpus_tfidf = tfidf[corpus]
-      
-      
-      lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=4)
-      
-      corpus_lsi = lsi[corpus_tfidf]
+            
+            
+         if len(tweet_array) < 10:
+            continue
+         
+         texts = [[word for word in document.lower().split() if len(word) > 4] for document in tweet_array]
+            
+         frequency = defaultdict(int)
+         for text in texts:
+            for token in text:
+               frequency[token] += 1
+               
+         texts = [[token for token in text if frequency[token] > 1] for text in texts]
+         
+         dictionary = corpora.Dictionary(texts)
    
+         corpus = [dictionary.doc2bow(text) for text in texts]
+   
+         tfidf = models.TfidfModel(corpus)
+         
+         corpus_tfidf = tfidf[corpus]
+         
+         
+         lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=4)
+         
+         corpus_lsi = lsi[corpus_tfidf]
       
-      for index, elem in enumerate(lsi.projection.s):
-         print index
-         if(elem > 1.9):
-            print "in category: "+str(cat_id)+" we had a trending topic at index: "+str(index)
-            for tweet_index, doc in enumerate(corpus_lsi): # both bow->tfidf and tfidf->lsi transformations are actually executed here, on the fly
-               for score_tuple in doc:
-                  if(score_tuple[0] == index):
-                     if(score_tuple[1] > .8):
-                        print tweets[tweet_index]
+         
+         for index, elem in enumerate(lsi.projection.s):
+            print index
+            if(elem > 1.9):
+               print "in category: "+str(cat_id)+" we had a trending topic at index: "+str(index)
+               trend_count = 0
+               included_tweets = []
+               for tweet_index, doc in enumerate(corpus_lsi): # both bow->tfidf and tfidf->lsi transformations are actually executed here, on the fly
+                  for score_tuple in doc:
+                     if(score_tuple[0] == index):
+                        if(score_tuple[1] > .8):
+                           trend_count += tweets[tweet_index]["tweet_count"]
+                           included_tweets.append(tweets[tweet_index])
+                           
+               
+            
+            if((trend_count / float(total_count)) > .5):
+               finished = True
+               print "in category: "+str(cat_id)
+               print "we had a tweet cluster: "
+               print included_tweets
+               
+               
+         if(finished):
+            break
+   
+            
+   
+               
                         
             
          
