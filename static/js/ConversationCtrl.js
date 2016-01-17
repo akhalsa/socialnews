2b7ede21e5877dee7034b8184fcf9b83284514c0
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-17 15:57:04 -0600

    hows this do?

--- static/js/ConversationCtrl.js
+++ static/js/ConversationCtrl.js
@@ -1,11 +1,12 @@
 app.controller("ConversationCtrl", function($scope, $http, $sce, $window) {
     
+    $scope.conversations = []
     
     $http.get("/api/conversations")
     .then(function(response) {
-        convos = response.data;
-        for(i=0; i<convos.length; i++ ){
-            conversation = convos[i];
+        $scope.conversations = response.data;
+        for(i=0; i<$scope.conversations.length; i++ ){
+            conversation = $scope.conversations[i];
             console.log("in cat: "+conversation.category_name+" we had a conversation around: "+conversation.representative_tweet);
         }
     });
