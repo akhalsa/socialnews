e9dacb1936e0da3b9b2ff9089a19864051f562ad
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-17 14:07:26 -0600

    ok don't need anything here

--- static/js/filtraCtrl.js
+++ static/js/filtraCtrl.js
@@ -47,18 +47,6 @@ app.controller("filtraCtrl", function($scope, $http, $sce, $window) {
         
     });
     
-    // Function to replicate setInterval using $timeout service.
-    $scope.intervalFunction = function(){
-      $timeout(function() {
-          console.log("loading tweets!");
-          loadTweets();
-          $scope.intervalFunction();
-      }, 30000);
-    };
-    
-    // Kick off the interval
-    $scope.intervalFunction();
-    
     
     // Configure user selections
     
