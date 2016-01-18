5edf8d0bf01dc22850fb4e1e5109bfcc7e04f40f
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-18 13:22:06 -0600

    tweet sections loaded

--- static/js/mainCtrl.js
+++ static/js/mainCtrl.js
@@ -19,6 +19,7 @@ app.controller("mainCtrl", function($scope, $http, $sce, $window) {
     function loadTweets(){
         $scope.display_sections = [];
         //first iterate through category structure
+        var completion_count = 0;
         for(i=0; i<$scope.category_structure.length; i++){
             //now what does the
             var cat = $scope.category_structure[i];
@@ -26,8 +27,12 @@ app.controller("mainCtrl", function($scope, $http, $sce, $window) {
             console.log("fetching from: "+endPoint);
             $http.get(endPoint)
             .then(function(response) {
+                completion_count++;
                 $scope.display_sections.push({"category": cat, "tweets":response.data});
                 console.log("display sections: "+$scope.display_sections);
+                if (completion_count  == ($scope.category_structure.length-1) ) {
+                    console.log("finished loading tweet sections");
+                }
             });
         }
         
