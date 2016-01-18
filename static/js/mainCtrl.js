d3426104bb8493958d92dd9de7bb380d437ecbb8
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-18 14:17:30 -0600

    does this sort?

--- static/js/mainCtrl.js
+++ static/js/mainCtrl.js
@@ -1,6 +1,8 @@
 app.controller("mainCtrl", function($scope, $http, $sce, $window) {
     $scope.category_structure = [];
     
+    $scope.tweet_sections = [];
+    
     $scope.display_sections = [];
     
     $http.get("/category")
@@ -17,7 +19,7 @@ app.controller("mainCtrl", function($scope, $http, $sce, $window) {
     
     //private methods
     function loadTweets(){
-        $scope.display_sections = [];
+        $scope.tweet_sections = [];
         //first iterate through category structure
         var completion_count = 0;
         for(i=0; i<$scope.category_structure.length; i++){
@@ -28,17 +30,39 @@ app.controller("mainCtrl", function($scope, $http, $sce, $window) {
             $http.get(endPoint)
             .then(function(response) {
                 completion_count++;
-                $scope.display_sections.push({"category": cat, "tweets":response.data});
-                console.log("display sections: "+$scope.display_sections);
+                $scope.tweet_sections.push({"category": cat, "tweets":response.data});
+                console.log("display sections: "+$scope.tweet_sections);
                 if (completion_count  == ($scope.category_structure.length) ) {
-                    console.log("finished loading tweet sections");
-                }else{
-                    console.log("completion_count is now: "+completion_count);
+                    for (j = 0; j<$scope.tweet_sections.length; j++) {
+                        console.log("section: "+$scope.tweet_sections[j].name);
+                    }
+                    $scope.tweet_sections.sort(compare);
+                    for (j = 0; j<$scope.tweet_sections.length; j++) {
+                        console.log("section: "+$scope.tweet_sections[j].name);
+                    }
                 }
             });
         }
-        
-        
-        
     }
+    
+
+    
+    function compare(a,b) {
+        a_total = 0;
+        b_total = 0;
+        for (i=0; i<a["tweets"].length; i++){
+            a_total += a["tweets"][i].tweet_count;
+        }
+        
+        for(i =0; i<b["tweets"].length; i++){
+            b_total += b["tweets"][i].tweet_count;
+        }
+        
+        if (a_total < b_total)
+          return -1;
+        else if (a_total > b_total)
+          return 1;
+        else 
+          return 0;
+      }
 });
