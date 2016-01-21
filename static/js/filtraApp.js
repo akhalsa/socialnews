67b23b686ca42abc5ace1a8560a5877ef082ec53
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-20 21:31:29 -0600

    maybe get those right?

--- static/js/filtraApp.js
+++ static/js/filtraApp.js
@@ -13,12 +13,12 @@ app.filter('eliminateLink', function($sce) {
     return function(text) {
         var urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
         console.log("checking");
-        if (typeof(text) != "undefined")  {
+        if (typeof(text) == "undefined")  {
             console.log("undefined text");
             return $sce.trustAsHtml(text);
         }
         var res = text.match(urlRegEx);
-        if (typeof(res) != "undefined") {
+        if (typeof(res) == "undefined") {
             return $sce.trustAsHtml(text);
         }
         text = text.replace(urlRegEx,"");
