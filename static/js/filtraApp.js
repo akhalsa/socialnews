e289a925986421f30a130c34abbaa2ff6937fadb
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-20 20:59:54 -0600

    oops

--- static/js/filtraApp.js
+++ static/js/filtraApp.js
@@ -13,9 +13,9 @@ app.filter('eliminateLink', function($sce) {
     return function(text) {
         var urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-]*)?\??(?:[\-\+=&;%@\.\w]*)#?(?:[\.\!\/\\\w]*))?)/g;
         //return $sce.trustAsHtml(text.replace(urlRegEx, ))
-        var res = str.match(urlRegEx);
+        var res = text.match(urlRegEx);
         text = text.replace(urlRegEx,"");
-        text = "<a href='"+res[0]+"'>"+text+"</a>";
+        text = "<a href=\""+res[0]+"\" target=\"_blank\">"+text+"</a>";
         return $sce.trustAsHtml(text);
         
     };
