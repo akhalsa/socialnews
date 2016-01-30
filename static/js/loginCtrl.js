d10967fc1ad18d6f99bef6c171148971147ef2df
Avtar Khalsa <akhalsa43@gmail.com>
2016-01-29 22:15:07 -0600

    can we include a username

--- static/js/loginCtrl.js
+++ static/js/loginCtrl.js
@@ -1,12 +1,14 @@
 app.controller("loginCtrl", function($scope, $http, $sce, $window) {
     $scope.email = "";
     $scope.password = "";
+    $scope.username="";
     $scope.createAccount = function(email, password){
         console.log("email: "+email);
         console.log("password: "+password);
         var data = {};
         data["email"] = email;
         data["password"] = password;
+        data["username"] = username;
         $http.post('/api/login', data).then(function(response) {
             console.log("successful response");
             console.log("response token:" + response.data.token);
