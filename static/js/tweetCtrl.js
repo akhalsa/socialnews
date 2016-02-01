56d4c1c4902f3013d3816a200cd6a827dcd26f9a
Rachel Senterfeit <senterfeitrd@gmail.com>
2016-02-01 00:34:24 -0600

    load tweet info

--- static/js/tweetCtrl.js
+++ static/js/tweetCtrl.js
@@ -2,6 +2,10 @@ app.controller("tweetCtrl", function($scope, $http, $sce, $window) {
     $scope.$watch('tweet_id', function () {
         if (typeof $scope.tweet_id != 'undefined' ) {
             console.log($scope.tweet_id);
+            $http.get("/api/tweet/"+$scope.tweet_id)
+            .then(function(response) {
+              console.log("successfully called");  
+            });
         }
         
         
