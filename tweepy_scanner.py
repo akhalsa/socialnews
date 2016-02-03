785229d6c99ffdfbcc161d948ae4a4708cd44105
Avtar Khalsa <akhalsa43@gmail.com>
2016-02-02 21:13:52 -0600

    create sentinal

---
+++ tweepy_scanner.py
@@ -0,0 +1,44 @@
+import MySQLdb
+
+from tornado.options import define, options, parse_command_line
+
+
+define("mysql_host", default="0", help="Just need the end point", type=int)
+
+host_live = "avtar-news-db-2.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
+host_dev = "avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
+host_target = host_live
+    
+    
+    
+if __name__ == '__main__':
+        
+    parse_command_line()
+    if(options.mysql_host == 0):
+        host_target = host_live
+    elif(options.mysql_host == 1):
+        host_target = host_dev
+        
+    global db
+
+    db = MySQLdb.connect(
+        host=host_target,
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
+    
+    while True:
+        #ok we need to check to make sure the most recent tweet is within the past hour
+        cursor =db.cursor()
+        sql = "SELECT ID, timestamp FROM Tweet ORDER BY timestamp DESC LIMIT 10;"
+        cursor.execute(sql)
+        shouldReboot = True        
+        if cursor.rowcount > 0:
+            row = cursor.fetchone()
+            tweet_id = row[0]
+            tweet_timestamp = row[1]
+            print tweet_timestamp
+            break
+        
