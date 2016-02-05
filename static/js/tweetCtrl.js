0652f4d63252b8ccc19ba8371d2bd7aa54cf3f21
Avtar Khalsa <akhalsa43@gmail.com>
2016-02-04 23:39:16 -0600

    maybe this works?

--- static/js/tweetCtrl.js
+++ static/js/tweetCtrl.js
@@ -1,11 +1,19 @@
 app.controller("tweetCtrl", function($scope, $http, $sce, $window) {
+    $scope.text = "";
+    $scope.handle = "";
+    $scope.comments = [];
+    
+    
     $scope.$watch('tweet_id', function () {
         if (typeof $scope.tweet_id != 'undefined' ) {
             console.log($scope.tweet_id);
             $http.get("/api/tweet/"+$scope.tweet_id)
             .then(function(response) {
               console.log("successfully called");
-              console.log(response.data.text);
+              $scope.text = response.data.text;
+              $scope.handle = response.data.twitter_handle;
+              $scope.comments = response.data.comments;
+              
             });
         }
         
