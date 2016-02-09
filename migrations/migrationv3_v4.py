f6dbe6a7e09eb1f1b8ff0a639b23da2d1e8e6eda
Avtar Khalsa <akhalsa43@gmail.com>
2016-02-08 23:22:14 -0600

    does this forward and backward migrate?

--- migrations/migrationv3_v4.py
+++ migrations/migrationv3_v4.py
@@ -26,36 +26,55 @@ def executeSql(db, sql):
         
         
 def forward():
-    if(sys.argv[1] == "0"):
-        host_target = host_live
-    elif(sys.argv[1] == "1"):
-        host_target = host_dev
-    
-    db = MySQLdb.connect(
-        host=host_target,
-        user="akhalsa",
-        passwd="sophiesChoice1",
-        db="newsdb",
-        charset='utf8',
-        port=3306)
-    
-    sql = "ALTER TABLE SourceCategoryRelationship ADD event_multiplier INT;"
-    executeSql(db, sql)
+        if(sys.argv[1] == "0"):
+            host_target = host_live
+        elif(sys.argv[1] == "1"):
+            host_target = host_dev
+        
+        db = MySQLdb.connect(
+            host=host_target,
+            user="akhalsa",
+            passwd="sophiesChoice1",
+            db="newsdb",
+            charset='utf8',
+            port=3306)
+        
+        sql = "ALTER TABLE SourceCategoryRelationship ADD event_multiplier INT;"
+        executeSql(db, sql)
+        
+        sql = "select table_name FROM information_schema.tables  WHERE table_name LIKE 'Occurrence_%';"
+        cur = db.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        cur.close()
+        for row in rows:
+                sql = "ALTER TABLE "+str(row[0])+" ADD occurrence_value INT;"
+                executeSql(db, sql)
+        
     
     
 def backward():
-    if(sys.argv[1] == "0"):
-        host_target = host_live
-    elif(sys.argv[1] == "1"):
-        host_target = host_dev
-    
-    db = MySQLdb.connect(
-        host=host_target,
-        user="akhalsa",
-        passwd="sophiesChoice1",
-        db="newsdb",
-        charset='utf8',
-        port=3306)
-    
-    sql = "ALTER TABLE SourceCategoryRelationship DROP event_multiplier;"
-    executeSql(db, sql)
+        if(sys.argv[1] == "0"):
+            host_target = host_live
+        elif(sys.argv[1] == "1"):
+            host_target = host_dev
+        
+        db = MySQLdb.connect(
+            host=host_target,
+            user="akhalsa",
+            passwd="sophiesChoice1",
+            db="newsdb",
+            charset='utf8',
+            port=3306)
+        
+        sql = "ALTER TABLE SourceCategoryRelationship DROP event_multiplier;"
+        executeSql(db, sql)
+        
+        sql = "select table_name FROM information_schema.tables  WHERE table_name LIKE 'Occurrence_%';"
+        cur = db.cursor()
+        cur.execute(sql)
+        rows = cur.fetchall()
+        cur.close()
+        for row in rows:
+                sql = "ALTER TABLE "+str(row[0])+" DROP occurrence_value;"
+                executeSql(db, sql)
