9322fe776e1ac6f8aad25e23cab6fe780770e805
Avtar Khalsa <akhalsa43@gmail.com>
2016-02-08 21:10:53 -0600

    new migration

---
+++ migrations/migrationv3_v4.py
@@ -0,0 +1,61 @@
+#This is a migration from 7a9e15e830e7b82856 to next commit
+import MySQLdb
+
+import sys
+
+
+
+host_live = "avtar-news-db-2.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
+host_dev = "avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com"
+host_target = host_live
+
+def executeSql(db, sql):
+        cursor = db.cursor()
+        try:
+                # Execute the SQL command
+                cursor.execute(sql)
+                # Commit your changes in the database
+                db.commit()
+        except Exception,e:
+                # Rollback in case there is any error
+                print "error on insertion of occurrence"
+                print str(e)
+                db.rollback()
+        lastRow = cursor.lastrowid
+        cursor.close()
+        
+        
+def forward():
+    if(sys.argv[1] == "0"):
+        host_target = host_live
+    elif(sys.argv[1] == "1"):
+        host_target = host_dev
+    
+    db = MySQLdb.connect(
+        host=host_target,
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
+    
+    sql = "ALTER TABLE SourceCategoryRelationship ADD event_multiplier INT;"
+    executeSql(db, sql)
+    
+    
+def backward():
+    if(sys.argv[1] == "0"):
+        host_target = host_live
+    elif(sys.argv[1] == "1"):
+        host_target = host_dev
+    
+    db = MySQLdb.connect(
+        host=host_target,
+        user="akhalsa",
+        passwd="sophiesChoice1",
+        db="newsdb",
+        charset='utf8',
+        port=3306)
+    
+    sql = "ALTER TABLE SourceCategoryRelationship DROP event_multiplier;"
+    executeSql(db, sql)
