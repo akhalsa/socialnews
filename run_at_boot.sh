954439d7824390680e5b2c5bb675a268a60740f8
Avtar Khalsa <akhalsa43@gmail.com>
2016-02-10 22:43:15 -0600

    ok try this

--- run_at_boot.sh
+++ run_at_boot.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
+#! /bin/sh
+# /etc/init.d/run_at_boot.sh
 
-pushd /home/ubuntu/socialnews/
-python tornado_host.py --mysql_host=1 &
-python tweepy_scanner.py --mysql_host=1 &
-python data_processing.py --mysql_host=1 &
-popd
+case "$1" in
+  start)
+    echo "**************Filtra Deployment Start"
+    # run application you want to start
+    cd /home/ubuntu/socialnews/
+    python tornado_host.py --mysql_host=1 &
+    python tweepy_scanner.py --mysql_host=1 &
+    python data_processing.py --mysql_host=1 &
+    echo "******************Filtra DEPLOYED"
+    ;;
+  stop)
+    echo "Stopping example"
+    # kill application you want to stop
+    killall python
+    ;;
+  *)
+    exit 1
+    ;;
+esac
+
+exit 0
