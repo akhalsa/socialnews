fdcb21ec2fd674f65f7555b5d4f388d5fbf7d6bf
Avtar Khalsa <akhalsa43@gmail.com>
2016-02-19 20:48:56 -0600

    fix the log

--- static/js/tweetCtrl.js
+++ static/js/tweetCtrl.js
@@ -60,7 +60,7 @@ app.controller("tweetCtrl", function($scope, $http, $sce, $window) {
         var data = {};
         data["comment_id"] = comment_id;
         data["vote_val"] = value;
-        console.log( "sending payload: "+str(data));
+        console.log( "sending payload: "+data);
         
         $http.post("/api/tweet/"+$scope.tweet_id+"/vote", data).then(function(response){
             console.log("post successful");
