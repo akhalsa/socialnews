5cc6067936b450597cca6ba16051a75b5425996b
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-09 23:47:00 -0600

    login service is getting there

--- static/js/loginService.js
+++ static/js/loginService.js
@@ -2,6 +2,10 @@ app.service('loginService', function(){
     this.logged_in = false;
     this.username = "";
     
+    this.invalid_creds = false;
+    this.showLogin = false;
+    this.showRegister = false;
+    
     this.createAccount = function(email, password, pw_confirm, username){
         
         if (password != pw_confirm) {
@@ -18,11 +22,80 @@ app.service('loginService', function(){
         
         
         $http.post("/api/signup", data).then(function successCallback(response){
-
+            console.log("successful response");
+            this.logged_in = true;
+            this.username = response.data.username;
+            this.dismissPopups();
         }, function errorCallback(response){
-
+            console.log("got an error");
+            if (response.status == 403) {
+                console.log("got a 403");
+            }
         });
         
+    }
+    
+    this.login = function(email, pw){
+        var data = {};
+    
+        data["email"] = email;
+        data["password"] = pw;
+
+        $http.post("/api/login", data).then(function successCallback(response){
+            this.logged_in = response.data.logged_in;
+            this.username = response.data.username;
+            this.dismissPopups();
+            
+        }, function errorCallback(response){
+            console.log("got an error");
+            if (response.status == 403) {
+                console.log("got a 403");
+                this.invalid_creds = true;
+            }
+        });
+        
+
+    }
+    
+    this.logout = function(){
+        var data = {};
+        console.log("logging out");
+        data["logout"] = true;
+        $http.put("/api/login", data).then(function(response){
+            if (response.status == 200) {
+                console.log("logout success");
+                this.logged_in = false;
+                this.username = "";
+            }else{
+                console.log("logout fail");
+            }
+        });
+    }
+    
+    this.checkLoggedIn = function(){
+        $http.get("/api/login")
+        .then(function(response) {
+            this.logged_in = response.data.logged_in;
+            this.username =response.data.username;
+        });
+    }
+    
+    this.dismissPopups = function (){
+        this.showLogin = false;
+        this.showRegister = false;
+        this.invalid_creds = false;
+    }
+    
+    this.showLoginPopup = function(){
+        this.showLogin = true;
+        this.showRegister = false;
+        this.invalid_creds = false;
+    }
+    
+    this.showRegisterPopup = function(){
+        this.showLogin = false;
+        this.showRegister = true;
+        this.invalid_creds = false;
     }
     
     
--- static/js/suggestionCtrl.js
+++ static/js/suggestionCtrl.js
@@ -17,76 +17,10 @@ app.controller("suggestionCtrl", function($scope, $http, $sce, $window, loginSer
     $scope.showRegister = false;
     $scope.throttled = false;
     
-    
     $scope.comment_rate_limit = false;
     
     
     
-    $scope.dismissPopups = function (){
-        $scope.showLogin = false;
-        $scope.showRegister = false;
-        $scope.invalid_creds = false;
-        $scope.throttled = false;
-        
-        $scope.login_email = "";
-        $scope.login_pw = "";
-        
-        
-        $scope.register_email = "";
-        $scope.register_username = "";
-        $scope.register_pw = "";
-        $scope.register_pw_confirm = "";
-    }
-    
-    $scope.showLoginPopup = function(){
-        $scope.showLogin = true;
-        $scope.showRegister = false;
-        $scope.invalid_creds = false;
-    }
-    
-    $scope.showRegisterPopup = function(){
-        $scope.showLogin = false;
-        $scope.showRegister = true;
-        $scope.invalid_creds = false;
-        $scope.throttled = false;
-    }
-    
-    $scope.login = function(){
-        var data = {};
-    
-        data["email"] = $scope.login_email;
-        data["password"] = $scope.login_pw;
-
-        $http.post("/api/login", data).then(function successCallback(response){
-            //$scope.logged_in = response.data.logged_in;
-            $scope.username = response.data.username;
-            $scope.dismissPopups();
-            //reloadPage();
-            
-        }, function errorCallback(response){
-            console.log("got an error");
-            if (response.status == 403) {
-                console.log("got a 403");
-                $scope.invalid_creds = true;
-            }
-        });
-        
-
-    }
-    
-    $scope.logout = function(){
-        var data = {};
-        console.log("logging out");
-        data["logout"] = true;
-        $http.put("/api/login", data).then(function(response){
-            if (response.status == 200) {
-                console.log("logout success");
-                //reloadPage();
-            }else{
-                console.log("logout fail");
-            }
-        });
-    }
 
     
     
