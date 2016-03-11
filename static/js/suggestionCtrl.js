037ac4170c59b0428da9edbb427b4dbf05f51638
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-10 23:35:37 -0600

    clear fields

--- static/js/suggestionCtrl.js
+++ static/js/suggestionCtrl.js
@@ -33,4 +33,28 @@ app.controller("suggestionCtrl", function($scope, $http, $sce, $window, loginSer
         loginService.createAccount($scope.register_email,  $scope.register_pw, $scope.register_pw_confirm, $scope.register_username);
     }
     
+    $scope.$watch('loginService.showRegister', function () {
+        reloadPage();
+    });
+    
+    $scope.$watch('loginService.showLogin', function () {
+        reloadPage();
+    });
+    
+    
+    function clearFields(){
+        $scope.login_email = "";
+        $scope.login_pw = "";
+        
+        
+        $scope.register_email = "";
+        $scope.register_username = "";
+        $scope.register_pw = "";
+        $scope.register_pw_confirm = "";
+    
+        $scope.invalid_creds = false;
+    }
+    
+    
+    
 });
