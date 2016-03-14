03b6cbec580dd5ce4d8c3ee7ba256505d1425162
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-13 23:08:25 -0500

    does this fail?

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -3,7 +3,7 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
+engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/sfsfsfsdb', echo=True)
 Session = sessionmaker(bind=engine)
 
 session = Session()
