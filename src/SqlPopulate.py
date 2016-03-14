c1fcb164d2616a9534eb45e5865d606956ae7d1e
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-14 00:36:07 -0500

    doesn't bode well

--- src/SqlPopulate.py
+++ src/SqlPopulate.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import sessionmaker
- 
+from sqlalchemy import create_engine
  
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 # Bind the engine to the metadata of the Base class so that the
