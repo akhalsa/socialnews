e0c78bc71c0498303fee359b46b6f80697a1e580
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-13 22:59:57 -0500

    test a little more

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -1,7 +1,7 @@
 import sqlalchemy
 
 from sqlalchemy import create_engine
-
-engine = create_engine('sqlite:///:memory:', echo=True)
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com', echo=True)
+Session = sessionmaker(bind=engine)
