188f5fa7ec464a418f1e77b74cfe1b7db46a35d0
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-13 23:16:59 -0500

    well come back to sqlalchemy

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -1,9 +1,6 @@
 from sqlalchemy import *
 
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
-Session = sessionmaker(bind=engine)
-
-session = Session()
 
 metadata = BoundMetaData(engine)
 
