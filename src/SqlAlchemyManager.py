13bcf3da67283c034e4a16573c030cb7fb4fb644
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-13 23:26:34 -0500

    does this create table

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -1,7 +1,23 @@
-from sqlalchemy import *
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+ 
+Base = declarative_base()
+ 
+class Suggestion(Base):
+    __tablename__ = 'Suggestion'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    text = Column(String(255), nullable=False)
+    
+ 
+ 
 
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 
-metadata = BoundMetaData(engine)
+Base.metadata.create_all(engine)
 
-print metadata
