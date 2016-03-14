7cb4eb1ce90c5155be25c88526f16213085643c3
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-13 23:49:20 -0500

    ok getting closer

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -4,6 +4,8 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+
+from sqlalchemy.dialects.mysql import TIMESTAMP
  
 Base = declarative_base()
  
@@ -13,6 +15,12 @@ class Suggestion(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     text = Column(String(255), nullable=False)
+    user_id = Column(Integer, nullable=False)
+    timestamp = Column(TIMESTAMP, nullable=False)
+    
+    def __repr__(self):
+        return "<Suggestion(id='%s', text='%s', userid='%s')>" % (
+                                self.id, self.text, self.user_id)
     
  
  
