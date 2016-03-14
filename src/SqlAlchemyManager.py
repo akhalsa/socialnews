fe647c4b3e1b80129453a42e9bb714b2cd559293
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-13 23:15:57 -0500

    switch import

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -1,7 +1,4 @@
-import sqlalchemy
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import *
 
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 Session = sessionmaker(bind=engine)
