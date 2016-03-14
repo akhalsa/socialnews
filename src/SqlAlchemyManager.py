c586b57645738acef21abb5c3587d13489016e07
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-13 23:02:35 -0500

    create session

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -5,3 +5,5 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com', echo=True)
 Session = sessionmaker(bind=engine)
+
+session = Session()
