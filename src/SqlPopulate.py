56d1a1a204369eadd4155fbeecc1879896fd5504
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-14 00:51:55 -0500

    does that help?

--- src/SqlPopulate.py
+++ src/SqlPopulate.py
@@ -1,10 +1,11 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
  
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
-
+Base = declarative_base()
 
 # create a configured "Session" class
 Session = sessionmaker(bind=engine)
