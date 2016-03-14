c4bfd8196c853745b000ad1391db56381059ae3a
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-14 00:39:33 -0500

    does this insert?

--- src/SqlPopulate.py
+++ src/SqlPopulate.py
@@ -4,21 +4,15 @@ from sqlalchemy import create_engine
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
-Base.metadata.bind = engine
- 
-DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
-session = DBSession()
- 
-# Insert a Person in the person table
-new_suggestion = Suggestion(text="Hi I'm a Suggestion", user_id=3 )
-                        
-session.add(new_suggestion)
+
+
+# create a configured "Session" class
+Session = sessionmaker(bind=some_engine)
+
+# create a Session
+session = Session()
+
+# work with sess
+myobject = Suggestion("I'm a comment", 2)
+session.add(myobject)
 session.commit()
- 
