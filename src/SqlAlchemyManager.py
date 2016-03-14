34367d6341ee02f3245aa752390f73343f23bd46
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-13 23:14:34 -0500

    functioning maybe?

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -3,7 +3,11 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql://akhalsa:sophiesChoice2@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
+engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 Session = sessionmaker(bind=engine)
 
 session = Session()
+
+metadata = BoundMetaData(engine)
+
+print metadata
