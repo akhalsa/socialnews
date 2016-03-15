a230f4e272665708051a115c61f26cdf7401ddbf
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-14 23:01:45 -0500

    alchemy should be working

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -3,8 +3,16 @@ import sys
 import Suggestion
 import Base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker
     
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 
 Base.Base.metadata.create_all(engine)
 
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+suggestion = Suggestion.Suggestion(text="first alchemy suggestion", user_id="21")
+session.add(suggestion)
+session.commit()
