084b6aa661c88bf7885b00ac05351d1c3da8be66
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-14 22:32:50 -0500

    create suggestion class

---
+++ src/Suggestion.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.mysql import TIMESTAMP
+
+Base = declarative_base()
+ 
+class Suggestion(Base):
+    __tablename__ = 'Suggestion'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    text = Column(String(255), nullable=False)
+    user_id = Column(Integer, nullable=False)
+    timestamp = Column(TIMESTAMP, nullable=False)
+    
+    
+    def __repr__(self):
+        return "<Suggestion(id='%s', text='%s', userid='%s')>" % (
+                                self.id, self.text, self.user_id)
+    
