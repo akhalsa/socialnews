323fe5e8992549513e313ab6df20b20ce530eae0
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-14 22:53:38 -0500

    more like the other

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -1,10 +1,10 @@
 import os
 import sys
 import Suggestion
-from Base import Base
+import Base
 from sqlalchemy import create_engine
     
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 
-Base.metadata.create_all(engine)
+Base.Base.metadata.create_all(engine)
 
---
+++ src/Suggestion.py
@@ -0,0 +1,20 @@
+from Base import Base
+
+from sqlalchemy.dialects.mysql import TIMESTAMP
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+class Suggestion(Base):
+    __tablename__ = 'Suggestion'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    text = Column(String(255), nullable=False)
+    user_id = Column(Integer, nullable=False)
+    timestamp = Column(TIMESTAMP, nullable=False)
+    
+    
+    def __repr__(self):
+        return "<Suggestion(id='%s', text='%s', userid='%s')>" % (
+                                self.id, self.text, self.user_id)
+
