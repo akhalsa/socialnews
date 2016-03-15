df871dd313d07e0aacd25b492d4d42412378ccc8
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-14 22:35:40 -0500

    fix imports

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -1,13 +1,9 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 import Suggestion.py
-    
- 
- 
 
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 
--- src/Suggestion.py
+++ src/Suggestion.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.mysql import TIMESTAMP
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
  
