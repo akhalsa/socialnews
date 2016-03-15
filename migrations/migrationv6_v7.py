00bf7fbd4da628044e878e7813344c2c4a98be26
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-14 23:54:49 -0500

    run it from migration

---
+++ migrations/migrationv6_v7.py
@@ -0,0 +1,16 @@
+import src.Suggestion
+import src.Base
+
+engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
+
+def forward():
+    Base.Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    
+    suggestion = Suggestion.Suggestion(text="first alchemy suggestion", user_id="21")
+    session.add(suggestion)
+    session.commit()
+def backward():
+    Suggestion.Suggestion.__table_name__.drop(engine)
