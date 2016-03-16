d71db1a9b7f3531647c2fd131a32534015dba253
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-15 23:49:05 -0500

    data seeding

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -7,8 +7,6 @@ from sqlalchemy.orm import relationship, backref, sessionmaker
     
 engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
 
-Base.Base.metadata.create_all(engine)
-
 Session = sessionmaker(bind=engine)
 session = Session()
 
