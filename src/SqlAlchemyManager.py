444e588224285982a6bd3db7146486ba2f400efb
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-19 09:44:13 -0500

    switch import

--- src/SqlAlchemyManager.py
+++ src/SqlAlchemyManager.py
@@ -1,43 +1,40 @@
 import os
 import sys
-import Suggestion as su
 import User
 import Base
 import datetime
 import SuggestionVote
+from Suggestion import Suggestion
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship, backref, sessionmaker
     
-
+engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
+Session = sessionmaker(bind=engine)
 
 
 def insertComment(suggestion_text, uid):
-    engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
-    Session = sessionmaker(bind=engine)
     session = Session()
-    suggestion = su.Suggestion(text=suggestion_text, user_id=uid, score=0)
+    suggestion = Suggestion(text=suggestion_text, user_id=uid, score=0)
     session.add(suggestion)
     session.commit()
     
     
 def fetchAllComments(uid):
-    engine = create_engine('mysql://akhalsa:sophiesChoice1@avtar-news-db-dev.cvnwfvvmmyi7.us-west-2.rds.amazonaws.com/newsdb', echo=True)
-    Session = sessionmaker(bind=engine)
     session = Session()
-    q = session.query(User.User, su.Suggestion).filter(User.User.ID == su.Suggestion.user_id).all()
+    q = session.query(User.User, Suggestion).filter(User.User.ID == Suggestion.user_id).all()
 
     response_json = []
     
-    for (User.User, su.Suggestion) in q:
+    for (User.User, Suggestion) in q:
         single_suggestion = {}
         if(User.User.username is not None):
             single_suggestion["user_name"] = User.User.username
         else:
             single_suggestion["username"] = "user"+str(User.User.ID)
             
-        single_suggestion["suggestion_text"] = su.Suggestion.text
-        single_suggestion["timestamp"] = (datetime.datetime.now() - su.Suggestion.timestamp).total_seconds()
-        single_suggestion["score"] = su.Suggestion.score
+        single_suggestion["suggestion_text"] = Suggestion.text
+        single_suggestion["timestamp"] = (datetime.datetime.now() - Suggestion.timestamp).total_seconds()
+        single_suggestion["score"] = Suggestion.score
         new_q = session.query(SuggestionVote.SuggestionVote).filter(uid == SuggestionVote.SuggestionVote.user_id).all()
         single_suggestion["vote_val"] = 0
         if(len(new_q) > 0):
