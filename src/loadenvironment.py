9dbd2af84d40bc2c7cb6509f5c5c3bb0e58869c9
Avtar Khalsa <akhalsa43@gmail.com>
2016-03-21 22:18:40 -0500

    ok this is a first settings file

---
+++ src/loadenvironment.py
@@ -0,0 +1,15 @@
+import json
+from pprint import pprint
+
+def getSqlEngineString():
+    file_location = "/home/ubuntu/settings.json"
+    
+
+    with open(file_location) as data_file:    
+        data = json.load(data_file)
+
+    pprint(data)
+
+
+
+getSqlEngineString()
